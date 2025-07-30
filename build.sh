8879726f57851b4fe6055a39db13042c12e894cf
gudrak <63885769+zionserver@users.noreply.github.com>
2025-07-30 13:23:29 -0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,78 +1,64 @@
 #!/bin/bash -e
 
-# Compilar LuaJIT
+# Clone Minetest 5.12
+if [ ! -d "minetest" ]; then
+  git clone --depth 1 --branch 5.12.0 https://github.com/minetest/minetest.git
+fi
+
+# Clone LuaJIT
+if [ ! -d "luajit" ]; then
+  git clone --depth 1 --branch v2.1 https://github.com/LuaJIT/LuaJIT.git
+fi
+
+# Compile LuaJIT
 pushd luajit
-make amalg -j$(nproc)
+make -j$(nproc) amalg
 popd
 
-# Compilar Luanti en modo servidor con soporte terminal
-pushd luanti
+# Build Minetest Server
+pushd minetest
 mkdir -p build
 cd build
 
-# Configuración para servidor con terminal
+# Server-only configuration
 cmake .. -G Ninja \
-    -DCMAKE_BUILD_TYPE=RelWithDebInfo \
-    -DCMAKE_INSTALL_PREFIX=/usr \
-    -DBUILD_SERVER=ON \
-    -DBUILD_CLIENT=OFF \
-    -DENABLE_CURSES=ON \
-    -DBUILD_UNITTESTS=OFF \
-    -DENABLE_SYSTEM_JSONCPP=OFF \
-    -DLUA_INCLUDE_DIR=../../luajit/src/ \
-    -DLUA_LIBRARY=../../luajit/src/libluajit.a \
-    -DZSTD_INCLUDE_DIR=/usr/include \          # Ruta explícita para Zstd
-    -DZSTD_LIBRARY=/usr/lib/x86_64-linux-gnu/libzstd.so
+  -DCMAKE_BUILD_TYPE=RelWithDebInfo \
+  -DCMAKE_INSTALL_PREFIX=/usr \
+  -DBUILD_SERVER=ON \
+  -DBUILD_CLIENT=OFF \
+  -DENABLE_CURSES=ON \
+  -DBUILD_UNITTESTS=OFF \
+  -DENABLE_SYSTEM_JSONCPP=OFF \
+  -DLUA_INCLUDE_DIR=../../luajit/src \
+  -DLUA_LIBRARY=../../luajit/src/libluajit.a
 
 ninja
 
-# Generar binario de depuración
-objcopy --only-keep-debug ../bin/minetestserver luanti-server.debug
+# Prepare DEB package
+mkdir -p pkg/usr/bin
+mkdir -p pkg/usr/share/minetest
+mkdir -p pkg/DEBIAN
 
-# Preparar para empaquetado .deb
-mkdir -p pkg-debian/DEBIAN
-mkdir -p pkg-debian/usr/bin
-mkdir -p pkg-debian/usr/lib
-mkdir -p pkg-debian/usr/share/luanti-server
+# Install server binary
+cp bin/minetestserver pkg/usr/bin/minetestserver
 
-# Instalar en directorio temporal
-DESTDIR=$(pwd)/pkg-debian ninja install
-
-# Mover el binario renombrado
-mv pkg-debian/usr/bin/minetestserver pkg-debian/usr/bin/luanti-server
-
-# Copiar bibliotecas requeridas
-cp /usr/lib/x86_64-linux-gnu/libzstd.so.1 pkg-debian/usr/lib/
-cp /usr/lib/x86_64-linux-gnu/libleveldb.so.1d pkg-debian/usr/lib/
-cp /usr/lib/x86_64-linux-gnu/libsnappy.so.1 pkg-debian/usr/lib/
-cp /usr/lib/x86_64-linux-gnu/libjsoncpp.so.24 pkg-debian/usr/lib/
-cp /usr/lib/x86_64-linux-gnu/libncursesw.so.6 pkg-debian/usr/lib/
-cp /usr/lib/x86_64-linux-gnu/libtinfo.so.6 pkg-debian/usr/lib/
-
-# Crear archivo de control para el paquete .deb
-cat > pkg-debian/DEBIAN/control <<EOF
-Package: luanti-server
-Version: 5.12.0
+# Create control file
+cat > pkg/DEBIAN/control <<EOF
+Package: minetest-server
+Version: 5.12.0-1
 Section: games
 Priority: optional
 Architecture: amd64
-Depends: libncurses6, libsqlite3-0, zlib1g, libgmp10, libcurl4, libc6, libstdc++6, libzstd1, libleveldb1d, libjsoncpp24
-Maintainer: Tu Nombre <tu@email.com>
-Description: Servidor Luanti con soporte de terminal
- Minetest modificado para servidor con interfaz terminal
+Depends: libc6, libstdc++6, libsqlite3-0, libzstd1, libcurl4, libncurses6, libgmp10, libleveldb1d, libjsoncpp25, zlib1g
+Maintainer: Minetest Team <minetest@example.com>
+Description: Minetest game server with terminal support
+ Minetest is an open source voxel game engine. This package provides
+ the server component with terminal administration interface.
+Homepage: https://www.minetest.net
 EOF
 
-# Crear script post-instalación
-cat > pkg-debian/DEBIAN/postinst <<EOF
-#!/bin/sh
-set -e
-chmod +x /usr/bin/luanti-server
-ldconfig  # Actualizar caché de bibliotecas
-EOF
-chmod +x pkg-debian/DEBIAN/postinst
-
-# Construir el paquete .deb
-dpkg-deb --build pkg-debian
-mv pkg-debian.deb luanti-server_5.12.0_amd64.deb
+# Build DEB package
+dpkg-deb --build pkg
+mv pkg.deb minetest-server_5.12.0_bookworm_amd64.deb
 
 popd
